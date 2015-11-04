571f86b5647a04afe18b3fcba8960d38dd0a65ea
Fan Chou <Chou.Fan@igt.com>
2015-11-04 18:53:21 +0800

    12.5.2 shoot The script can not be bind to player itself but to a sub instance as shootPos, otherwise the prefab would be exploded immediately after shooting

---
+++ Assets/_Scripts/shootPos.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootPos : MonoBehaviour {
+	public Rigidbody2D rocket;
+	public float speed = 20f;
+
+	private PlayerController playerController;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		playerController = transform.root.GetComponent<PlayerController>();
+		anim = transform.root.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			anim.SetTrigger ("Shoot");
+			Quaternion bulletDirection;
+			float bulletSpeed;
+			if (playerController.facingRight) {
+				bulletDirection = Quaternion.Euler (new Vector3(0, 0, 0));
+				bulletSpeed = speed;
+			}else{
+				bulletDirection = Quaternion.Euler (new Vector3(0, 0, 180));
+				bulletSpeed = -speed;
+			}
+
+			Rigidbody2D bulletInstance = Instantiate (rocket, transform.position, bulletDirection) as Rigidbody2D;
+			bulletInstance.velocity = new Vector2(bulletSpeed, 0);
+		}
+	}
+}
